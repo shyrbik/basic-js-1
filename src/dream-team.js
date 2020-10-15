a2240e8dd39e2fdf0b131627dd82f8e57c200e04
Elwa36s <24dds08@gmail.com>
2020-10-15 23:57:43 +0300

    init: start and finish Dream Team Task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(arr) {
+ // throw new CustomError('Not implemented');
+ if (Array.isArray(arr)) {
+ let letters = [];
+for (let i = 0; i < arr.length; i++) {
+  if (typeof(arr[i]) === 'string') {
+    let word = arr[i];
+    word = word.replace(/\s+/g, '');
+    letters.push(word[0].toUpperCase());
+  }
+}
+letters = letters.sort();
+let result = letters.join('');
+return result;
   // remove line with error and write your code here
+}
+return false
 };
