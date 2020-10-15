1d43ace69423f2b46395b3e1084f7fbb54e2bc53
Elwa36s <24dds08@gmail.com>
2020-10-15 23:34:12 +0300

    init: start and finish Count Cats Task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+//const CustomError = require("../extensions/custom-error");
+module.exports  = function countCats(Array) {
+  let cats = 0,
+      cat = '^^';
+  for (let i = 0; i < Array.length; i++) {
+    for (let j = 0; j < Array[i].length; j++) {
+      if (Array[i][j] === cat) {
+        cats += 1;
+      }
+    }
+  }
+return cats;
   // remove line with error and write your code here
 };
