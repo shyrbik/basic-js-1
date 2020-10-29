4db8039e4ff8bafb9b2c1b80b632721034da9a4a
Elwa36s <24dds08@gmail.com>
2020-10-29 21:27:47 +0300

    init: start repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+let arr = str.split(),
+    finalStr,
+    basicAdd,
+    times,
+    separator,
+    addStr,
+    timesAddStr,
+    addSeparator,
+    result;
+ (`repeatTimes` in options) ? times = options[`repeatTimes`] : times = 0;
+ console.log(times);
+ (`separator` in options) ? separator = options[`separator`].toString() : separator = '+';
+ (`addition` in options) ? addStr = options[`addition`].toString() : addStr = '';
+ (`additionRepeatTimes` in options) ? timesAddStr = options[`additionRepeatTimes`] : timesAddStr = 0;
+ (`additionSeparator` in options) ? addSeparator = options[`additionSeparator`].toString() : addSeparator = '|' ;
+  
+ if (addStr !== '') {
+   if (timesAddStr > 1) {
+    basicAdd = `${addStr}${addSeparator}`
+    for (let i = 0; i < timesAddStr; i++) {
+    basicAdd += basicAdd;
+    }
+    } else {
+    basicAdd = `${addStr}`
+    }
+  }
+  if (times !== 0) {
+    finalStr = `${str}${basicAdd}`
+    result = finalStr
+    for (let i = 1; i < times; i++) {
+      result = result + separator + finalStr
+    
+    }
+    
+  }
+  
+  return result;
+
+  }
+
+
+
+
   
