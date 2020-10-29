b112e6649771a8cedf66f1adc3f314ed24cae365
Elwa36s <24dds08@gmail.com>
2020-10-29 20:11:25 +0300

    fix: fix mistake, finish transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,56 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
+ 
 let i,
     thisArray = arr,
     doubleNext = `--double-next`,
     doublePrev = `--double-prev`,
     discardNext = `--discard-next`,
-    discardPrev = `--discard-prev`;
-    
-    if (!(Array.isArray(thisArray))) throw Error;
-for (i = 0; i < thisArray.length; i++) {
-    let next = thisArray[i + 1];
-    let prev = thisArray[i - 1];
-    
-  if (thisArray[i] === doubleNext) {
-    if (i === (thisArray.length - 1)) {
-      thisArray.pop();
-    } else {
-    thisArray.splice(i, 1, next);
-    i -= i;
-    continue;
-  }
-  }
-  if (thisArray[i] === doublePrev) {
-    if (i === 0) {
-      thisArray.shift();
-    } else { 
-
-    thisArray.splice(i, 1, prev);
-  }
-  }
-  if (thisArray[i] === discardNext) {
-    if (i === (thisArray.length - 1)) {
-      thisArray.pop();
-      continue;
-    } else if ((thisArray[i + 2] === doublePrev) || (thisArray[i + 2] === discardPrev)) {
-    thisArray.splice(i, 3);
-    }else {
-    thisArray.splice(i, 2);
-    i -= i;
-    continue;
+    discardPrev = `--discard-prev`,
+    resultArr = [];
+     if (!(Array.isArray(thisArray))) throw Error;
+    for (i = 0; i < thisArray.length; i++) {
+      let next = thisArray[i + 1],
+          prev = thisArray[i - 1],
+          currentPos = thisArray[i];
+      switch (currentPos) {
+        case doubleNext:
+          if (i === (thisArray.length - 1)) {
+            break;
+          } else {
+            resultArr.push(next, next);
+            i++;
+            break;
+          }
+        case doublePrev:
+          if (i === 0) {
+            break;
+          } else { 
+            resultArr.push(prev)
+            break;
+        }
+        case discardNext:
+          if (i === (thisArray.length - 1)) {
+            break;
+          }
+          if ((thisArray[i + 2] === doublePrev) || (thisArray[i + 2] === discardPrev)) {
+          i += 2;
+          break;
+          }else {
+          i++
+          break;
+          }
+        case discardPrev:
+          if (i === 0) {
+            break;
+          } else {
+            resultArr.pop();
+            break;
+        }
+        default: 
+        resultArr.push(currentPos);
     }
   }
-  if (thisArray[i] === discardPrev) {
-    if (i === 0) {
-      thisArray.shift();
-    } else {
-    thisArray.splice(i - 1, 2);
-  }
-  }
+  return resultArr;
 }
-  return thisArray;
-};
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
