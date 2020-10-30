9d1460ec0debe57ae57c649ec59cd67ed2847ad8
Elwa36s <24dds08@gmail.com>
2020-10-30 18:54:35 +0300

    fix: clear task viginere and depth

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,19 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  constructor(bool){
-    if (bool == 'true' || bool == '') {
-      this.type = directMachine;
-    } else {
-      this.type = reverseMachine;
-    }
-  }
-  encrypt(word, key) {
-    if (this.type == 'true') {
-
-    } else {
-      
-    }
+  encrypt() {
+    throw new CustomError('Not implemented');
+    // remove line with error and write your code here
   }    
   decrypt() {
     throw new CustomError('Not implemented');
