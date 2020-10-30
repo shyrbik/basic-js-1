1a5f80a307e4a4eb032b64451ba15998e970949b
Elwa36s <24dds08@gmail.com>
2020-10-30 15:24:54 +0300

    init: start viginere task and recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+//module.exports = 
+class DepthCalculator {
+  calculateDepth(arr) {
+    let numb;
+  getArrayDepth(arr) {
+      if (Array.isArray(arr)) return numb = 1 + Math.max(...arr.map(child => getArrayDepth(child)))
+      else return numb = 0;
+  }
+  console.log(numb);
+  return numb;
   }
 };
+const instance = new DepthCalculator();
+
+console.log(instance.calculateDepth([1, 2, 3, 4, 5, [1]]));
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,9 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool){
+    if (bool == 'true' || bool == '') {
+      this.type = directMachine;
+    } else {
+      this.type = reverseMachine;
+    }
+  }
+  encrypt(word, key) {
+    if (this.type == 'true') {
+
+    } else {
+      
+    }
   }    
   decrypt() {
     throw new CustomError('Not implemented');
