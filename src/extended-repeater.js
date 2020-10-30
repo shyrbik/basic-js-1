f993b8e065b3e4d4f05dc3ba1d376ea8a4b24267
Elwa36s <24dds08@gmail.com>
2020-10-30 10:38:28 +0300

    fix: fix and finish repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,8 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-let arr = str.split(),
-    finalStr,
+let finalStr,
     basicAdd,
     times,
     separator,
@@ -10,38 +9,40 @@ let arr = str.split(),
     timesAddStr,
     addSeparator,
     result;
- (`repeatTimes` in options) ? times = options[`repeatTimes`] : times = 0;
- console.log(times);
- (`separator` in options) ? separator = options[`separator`].toString() : separator = '+';
- (`addition` in options) ? addStr = options[`addition`].toString() : addStr = '';
- (`additionRepeatTimes` in options) ? timesAddStr = options[`additionRepeatTimes`] : timesAddStr = 0;
- (`additionSeparator` in options) ? addSeparator = options[`additionSeparator`].toString() : addSeparator = '|' ;
-  
- if (addStr !== '') {
-   if (timesAddStr > 1) {
-    basicAdd = `${addStr}${addSeparator}`
-    for (let i = 0; i < timesAddStr; i++) {
-    basicAdd += basicAdd;
-    }
+ (`repeatTimes` in options) ? (times = options[`repeatTimes`]) : (times = 0);
+ (`separator` in options) ? (separator = `${options[`separator`]}`) : (separator = '+');
+ (`addition` in options) ? (addStr = `${options['addition']}`) : (addStr = '');
+ (`additionRepeatTimes` in options) ? (timesAddStr = options[`additionRepeatTimes`]) : (timesAddStr = 0);
+ (`additionSeparator` in options) ? (addSeparator = `${options[`additionSeparator`]}`) : (addSeparator = '|');
+ 
+let additionalString = '',
+      basicString = '',
+      extendetString = '';
+
+  if (addStr !== '') {
+    if (timesAddStr > 1) {
+      basicAdd = addStr;
+      additionalString = basicAdd;
+      for (let i = 1; i < timesAddStr; i++) {
+        additionalString = additionalString + addSeparator + basicAdd;
+      }
     } else {
-    basicAdd = `${addStr}`
+      basicAdd = addStr;
+      additionalString = basicAdd;
     }
+  } else {
+    additionalString = '';
   }
-  if (times !== 0) {
-    finalStr = `${str}${basicAdd}`
-    result = finalStr
+
+  if (times > 1) {
+    basicString = str + additionalString;
+    finalStr = basicString;
     for (let i = 1; i < times; i++) {
-      result = result + separator + finalStr
-    
+      finalStr = finalStr + separator + basicString;
     }
-    
+  } else {
+    finalStr = str + additionalString;
   }
-  
+  result = finalStr;
   return result;
-
-  }
-
-
-
-
-  
+}
