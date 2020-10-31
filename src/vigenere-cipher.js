8c70b68ba3a8f36cdbde920b076d6af8ebb27789
Elwa36s <24dds08@gmail.com>
2020-10-31 15:41:26 +0300

    fix: fix vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,98 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    if (type === false) {
+      this.type = 'reverse';
+    } else {
+      this.type = 'direct';
+    }
   }
-}
+ 
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw Error();
+    let alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let keyLong = key;
+    let cryptedArr = [];
+    while (keyLong.length < message.length) {
+      keyLong += key;
+    }
+    let messageArray = message.toUpperCase().split('');
+    let keyArray = keyLong.toUpperCase().split('');
+   
+    for (let i = 0; i < messageArray.length; i++) {
+       function checkIndexMes(index) {
+    return index === messageArray[i]
+    }
+      let messageNumb = alfabet.findIndex(checkIndexMes);
+      if (messageNumb === -1) {
+        keyArray.splice(i, 0, messageArray[i]);
+        cryptedArr.push(messageArray[i]);
+      } else {
+        function checkIndexKey(index) {
+          return index === keyArray[i]
+          }
+        let keyNumb = alfabet.findIndex(checkIndexKey);
+        let sum = messageNumb + keyNumb;
+        //console.log(`i = ${i}, symb = ${messageArray[i]}, messageNumb = ${messageNumb}, keyNumb = ${keyNumb}, sum = ${sum}`)
+        while (sum > 25) {
+          sum = Math.abs(sum - 26);
+        }
+        cryptedArr.push(alfabet[sum]);
+      }
+    }
+    if (this.type === 'direct') {
+      return cryptedArr.join('');
+    } else {
+      let reversedArr = [];
+      for (let i = cryptedArr.length; i >= 0; i++){
+      reversedArr.push(cryptedArr[i]);
+      return cryptedArr.join('');  
+      }
+    }
+  }    
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw Error();
+    let alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let keyLong = key;
+    let deCryptedArr = [];
+    while (keyLong.length < message.length) {
+      keyLong += key;
+    }
+    let messageArray = message.split('');
+    let keyArray = keyLong.split('');
+    for (let i = 0; i < messageArray.length; i++) {
+      function checkIndexMes(index) {
+        return index === messageArray[i]
+        }
+      let messageNumb = alfabet.findIndex(checkIndexMes);
+      if (messageNumb === -1) {
+        keyArray.splice(i, 0, messageArray[i]);
+        deCryptedArr.push(messageArray[i]);
+      } else {
+        function checkIndexKey(index) {
+          return index === keyArray[i]
+          }
+        let keyNumb = alfabet.findIndex(checkIndexKey);
+        let sum = Math.abs(messageNumb - keyNumb);
+        while (sum > 25) {
+          sum = Math.abs(sum - 26);
+        }
+        deCryptedArr.push(alfabet[sum]);
+      }
+    }
+    if (this.type === 'direct') {
+      return deCryptedArr.join('');
+    } else {
+      let reversedArr = [];
+      for (let i = deCryptedArr.length; i >= 0; i++){
+      reversedArr.push(deCryptedArr[i]);
+      return deCryptedArr.join('');  
+      }
+    }
+  }    
+  }
+  //const directMachine = new VigenereCipheringMachine();
+ // console.log(directMachine.encrypt('Samelengthkey', 'Samelengthkey'));  'KAYIWIAMMOUIW';
 
 module.exports = VigenereCipheringMachine;
