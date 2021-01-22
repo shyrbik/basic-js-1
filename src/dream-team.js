74c9e5276ab5198d4998b8756f8de9f39e68fefe
PaulRomanov <ramosha@mail.ru>
2021-01-22 12:45:53 +0300

    add final code task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arr) {
+    if (arr == undefined || arr == null) {
+      return false;
+    }
+  
+    let name = '';
+  
+    for (i = 0; i < arr.length; i++) {
+        if (typeof (arr[i]) == typeof (String())) {
+        name +=  arr[i].trim()[0];
+      }
+    }
+  
+    name = name.toUpperCase().split("").sort().join("");
+  
+    return name;
+  };
