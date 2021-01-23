3e0b140c514627f7206dbd3b2ba9bc5198d9b5ed
PaulRomanov <ramosha@mail.ru>
2021-01-23 15:36:41 +0300

    init: start task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) throw Error;
+
+  let newArr = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--double-next' && arr[i + 1] !== undefined) {
+      newArr.push(arr[i + 1]);
+
+    } else if (arr[i] === '--double-prev' && arr[i - 1] !== undefined && arr[i - 2] !== "--discard-next") {
+      newArr.push(arr[i - 1]);
+
+    } else if (arr[i] === '--discard-next' && arr[i + 1] !== undefined) {
+      i++;
+
+    } else if (arr[i] === '--discard-prev' && arr[i - 1] !== undefined && arr[i - 2] !== "--discard-next") {
+      newArr.pop();
+      
+    } else if (arr[i] !== '--double-next' && arr[i] !== '--double-prev' && arr[i] !== '--discard-next' && arr[i] !== '--discard-prev') {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
