49d0b8613de413ff87032aa09390e469bd878677
PaulRomanov <ramosha@mail.ru>
2021-01-25 11:52:42 +0300

    feat: add new code

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,87 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse) {
+    this.reverse = reverse;
+  }
+
+  encrypt(message, key) {
+    if(!message || !key) {
+      throw new Error()
+    }
+
+    let alphabet = [];
+    for (let i = 65; i < 91; i++) {
+      alphabet.push( String.fromCharCode(i) );
+    }
+
+    let m = message.toUpperCase().split('');
+    let k = key.toUpperCase().split('');
+
+    let result = [];
+    let j = 0;
+
+    for (let i = 0; i < m.length; i++) {
+      if(alphabet.indexOf(m[i]) === -1) {
+        result.push(m[i]);
+        continue;
+      }
+
+      let sum = alphabet.indexOf(m[i]) + alphabet.indexOf(k[j]);
+      let finIndex = -1;
+
+      if (alphabet.length - 1 - sum < 0) {
+        finIndex = Math.abs(alphabet.length - 1 - sum) - 1;
+      } else {
+        finIndex = sum;
+      }
+
+      result.push(alphabet[finIndex]);
+
+      j++;
+      if (j === k.length) j = 0;
+    }
+
+    return (this.reverse === false) ? result.reverse().join('') : result.join('');
+  }
+
+  decrypt(message, key) {
+    if( !message || !key ) {
+      throw new Error();
+    }
+
+    let alphabet = [];
+    for (let i = 65; i < 91; i++) {
+      alphabet.push( String.fromCharCode(i) );
+    }
+
+    let m = message.toUpperCase().split('');
+    let k = key.toUpperCase().split('');
+
+    let result = [];
+    let j = 0;
+
+    for (let i = 0; i < m.length; i++) {
+      if(alphabet.indexOf(m[i]) === -1) {
+        result.push(m[i]);
+        continue;
+      }
+
+      let mI = alphabet.indexOf(m[i]);
+      let kI = alphabet.indexOf(k[j]);
+
+      let finIndex = 1 + (mI > kI) ? mI - kI : kI - mI;
+      if(finIndex < 0) {
+        finIndex = alphabet.length + finIndex;
+      }
+
+      result.push(alphabet[finIndex]);
+
+      j++;
+      if (j === k.length) j = 0;
+    }
+
+    return (this.reverse === false) ? result.reverse().join('') : result.join('');
   }
 }
 
